1ef7ec9dcbb9379f2f19548359804a262866dd42
keneanung <keneanung@googlemail.com>
2021-11-24 21:06:59 +0100

    Add nexus function class

---
+++ src/classes/function.ts
@@ -0,0 +1,46 @@
+import { readScriptFileRelativeToDefintion } from '../functionsInteractingWithFileSystem';
+import { IdGenerator } from '../utils';
+
+/**
+ * Class representing a function nexus reflex object.
+ */
+export class NexusFunction implements client.Function {
+  name = '';
+  id = 0;
+  type: 'function' = 'function';
+  enabled = true;
+  code = '';
+
+  /**
+   * Creates a complete function object from a partial one.
+   *
+   * @param {Partial<client.Function> & { codeFile?: string }} partialFunction A partial function object. This object might additional contain the codeFile property,
+   * which makes this function replace the code property with the content of the given file. The path of that file must
+   * be relative to the packageDefinitionFile.
+   * @param {IdGenerator} idGenerator The IdGenerator to use for retrieving the ID.
+   * @param {string} packageDefinitionFile The (absolute) path to the package definition file that this function pbject is built from.
+   */
+  constructor(
+    partialFunction: Partial<client.Function> & { codeFile?: string },
+    idGenerator: IdGenerator,
+    packageDefinitionFile: string,
+  ) {
+    this.id = idGenerator.getId();
+
+    if (partialFunction.name !== undefined) {
+      this.name = partialFunction.name;
+    }
+
+    if (partialFunction.enabled !== undefined) {
+      this.enabled = partialFunction.enabled;
+    }
+
+    if (partialFunction.code !== undefined) {
+      this.code = partialFunction.code;
+    }
+
+    if (partialFunction.codeFile !== undefined) {
+      this.code = readScriptFileRelativeToDefintion(partialFunction.codeFile, packageDefinitionFile);
+    }
+  }
+}
